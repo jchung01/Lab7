c0fcb015e09c705d557e35460684daab947e643d
jchung01 <jjchung276@gmail.com>
2021-05-15 00:41:19 -0700

    restore fetch function for service worker

--- sw.js
+++ sw.js
@@ -25,37 +25,37 @@ self.addEventListener('activate', function(event) {
     event.waitUntil(clients.claim());
 });
 
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//       caches.match(event.request)
-//         .then(function(response) {
-//           // Cache hit - return response
-//           if (response) {
-//             // console.log("found in cache");
-//             return response;
-//           }
-//           return fetch(event.request).then(
-//             function(response) {
-//               // Check if we received a valid response
-//               if(!response || response.status !== 200) {
-//                 return response;
-//               }
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            // console.log("found in cache");
+            return response;
+          }
+          return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200) {
+                return response;
+              }
   
-//               // IMPORTANT: Clone the response. A response is a stream
-//               // and because we want the browser to consume the response
-//               // as well as the cache consuming the response, we need
-//               // to clone it so we have two streams.
-//               var responseToCache = response.clone();
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
   
-//               caches.open(CACHE_NAME)
-//                 .then(function(cache) {
-//                   // console.log("added " + response + " entry to cache");
-//                   cache.put(event.request, responseToCache);
-//                 });
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  // console.log("added " + response + " entry to cache");
+                  cache.put(event.request, responseToCache);
+                });
   
-//               return response;
-//             }
-//           );
-//         })
-//       );
-//   });
+              return response;
+            }
+          );
+        })
+      );
+  });
